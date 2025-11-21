eb1ab7c6721b2f82d2f067e580f8f5d12417816e
Fran <fgrcalifa@gmail.com>
2025-11-21 20:06:18 +0100

    Creacion de landing-page, navbar y footer

---
+++ frontend/src/app/components/landing-page-component/landing-page-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landing-page-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing-page-component.html',
+  styleUrl: './landing-page-component.scss'
+})
+export class LandingPageComponent {
+  // Logic for the landing page can be added here if needed
+}
